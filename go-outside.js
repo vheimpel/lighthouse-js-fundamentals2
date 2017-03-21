313a04fba068fb75df772fcc6558e770f4c7bb47
Val Heimpel <vheimpel@gmail.com>
2017-03-21 15:17:53 -0400

    Adds conditionals exerises

---
+++ go-outside.js
@@ -0,0 +1,33 @@
+var raining = false;
+var cold = true;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+var temperature = 45;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+  console.log("Oh it's NOT raining")
+}
+
